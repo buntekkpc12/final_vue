a12aec89f05516e26e153890619d1767d46c7fc1
buntek <chhornbuntek@gmail.com>
2024-10-15 21:36:03 +0700

    hello

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import mysql.connector
 
 app = Flask(__name__)
 
+
 def get_db_connection():
     connection = mysql.connector.connect(
         host='localhost',
@@ -12,12 +13,11 @@ def get_db_connection():
     )
     return connection
 
+
 connection = get_db_connection()
 cursor = connection.cursor()
 
 import routes
-
-
 
 if __name__ == '__main__':
     app.run()
